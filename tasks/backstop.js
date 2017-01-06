f308f5442a999bc0cc8cf2f5c55477c66208d480
DavidMove <david.huang@move.com>
2017-01-06 11:21:45 -0800

    Updated code to work with latest backstopjs setup.

--- tasks/backstop.js
+++ tasks/backstop.js
@@ -50,7 +50,7 @@ module.exports = function(grunt) {
       };
 
       this.setup = function(backstop_path, test_path, cb) {
-        child_process.exec('cp -r ./bitmaps_test ./bitmaps_reference ' + backstop_path, {cwd: test_path}, function(err, stdout, stderr) {
+        child_process.exec('npm run genConfig', {cwd: backstop_path}, function(err, stdout, stderr) {
           this.log(err, stdout, stderr);
           cb();
         }.bind(this));
@@ -64,22 +64,16 @@ module.exports = function(grunt) {
       };
 
       this.run_tests = function(backstop_path, test_path, cb) {
-        child_process.exec('backstop test', {cwd: backstop_path}, function(err, stdout, stderr) {
+        child_process.exec('npm run test', {cwd: backstop_path}, function(err, stdout, stderr) {
           this.log(err, stdout, stderr);
-          child_process.exec('cp -rf ./backstop_data/bitmaps_test ' + test_path, {cwd: backstop_path}, function(err, stdout, stderr) {
-            this.log(err, stdout, stderr);
-            cb(true);
-          }.bind(this));
+          cb(true);
         }.bind(this));
       };
 
       this.create_references = function(backstop_path, test_path, cb) {
-        child_process.exec('backstop reference', {cwd: backstop_path}, function(err, stdout, stderr) {
+        child_process.exec('npm run reference', {cwd: backstop_path}, function(err, stdout, stderr) {
           this.log(err, stdout, stderr);
-          child_process.exec('cp -rf ./backstop_data/bitmaps_reference ' + test_path, {cwd: backstop_path}, function(err, stdout, stderr) {
-            this.log(err, stdout, stderr);
-            cb(true);
-          }.bind(this));
+          cb(true);
         }.bind(this));
       };
 
